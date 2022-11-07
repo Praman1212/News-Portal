346eca5df12401a8765f84ca75b41696e3338d63
Praman Ghimire <pramanghimire25@gmail.com>
2022-11-07 08:53:41 +0545

    Added headline section

---
+++ src/components/headline/Headline.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import './headline.css'
+import Stock from '../../assets/bitcoin.jpg'
+// import Lambo from '../../assets/Lambo.jpg'
+import Wordpress from '../../assets/20150429160658-website-traffic.jpg'
+
+const Headline = () => {
+    return (
+        <section className='Headline'>
+            <div className="headline-section">
+                <div className="headline-heading">
+                    <h2>Headline</h2>
+                </div>
+                <div className="headline-line">
+
+                </div>
+                <div className="headline-section-items">
+                    <div className="headline-section-item__1">
+                        <div className="flex-item__1">
+                            <div className="headline-text-section__1">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                            <div className="headline-image-section__1">
+                                <img src={Stock} alt="" />
+                            </div>
+
+                        </div>
+                        <div className="flex-item__1">
+                            <div className="headline-text-section__1">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                            <div className="headline-image-section__1">
+                                <img src={Stock} alt="" />
+                            </div>
+
+                        </div>
+                        <div className="flex-item__1">
+                            <div className="headline-text-section__1">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                            <div className="headline-image-section__1">
+                                <img src={Stock} alt="" />
+                            </div>
+
+                        </div>
+                        <div className="flex-item__1">
+                            <div className="headline-text-section__1">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                            <div className="headline-image-section__1">
+                                <img src={Stock} alt="" />
+                            </div>
+
+                        </div>
+                        <div className="flex-item__1">
+                            <div className="headline-text-section__1">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                            <div className="headline-image-section__1">
+                                <img src={Stock} alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="headline-section-item__2">
+                        <div className="headline-image-section__2">
+                            <img src={Wordpress} alt="" />
+                        </div>
+                        <div className="headline-text-section__2">
+                            <div className="headline-text-section__2">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4><br />
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, corrupti inventore! Voluptatum tempora suscipit saepe a ipsum earum exercitationem ducimus soluta obcaecati, voluptates sit, iusto molestias dolorem neque commodi nemo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa placeat dolor nihil voluptatibus maxime nisi minima at vero adipisci nulla totam distinctio repudiandae iure error ea assumenda, quisquam quas obcaecati?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni odit dolores maxime maiores ipsum voluptate animi nesciunt totam quae, modi quaerat porro repellendus minima! Corrupti vero mollitia deleniti at ex?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="headline-section-item__3">
+                        <div className="flex-item__3">
+                            <div className="headline-image-section__3">
+                                <img src={Stock} alt="" />
+                            </div>
+                            <div className="headline-text-section__3">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                        </div>
+                        <div className="flex-item__3">
+                            <div className="headline-image-section__3">
+                                <img src={Stock} alt="" />
+                            </div>
+                            <div className="headline-text-section__3">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                        </div>
+                        <div className="flex-item__3">
+                            <div className="headline-image-section__3">
+                                <img src={Stock} alt="" />
+                            </div>
+                            <div className="headline-text-section__3">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                        </div>
+                        <div className="flex-item__3">
+                            <div className="headline-image-section__3">
+                                <img src={Stock} alt="" />
+                            </div>
+                            <div className="headline-text-section__3">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                        </div>
+                        <div className="flex-item__3">
+                            <div className="headline-image-section__3">
+                                <img src={Stock} alt="" />
+                            </div>
+                            <div className="headline-text-section__3">
+                                <h4>यु–१५ महिला च्याम्पियनसिप फुटबल :  आज नेपाल र बङ्गलादेश</h4>
+                                <p>access_time१३ मिनेट अघि</p>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Headline
