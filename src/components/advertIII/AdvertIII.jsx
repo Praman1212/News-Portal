b6996ec3d2f850e58b762544aca0f905fa9acd7c
Praman Ghimire <pramanghimire25@gmail.com>
2022-11-07 12:44:33 +0545

    Added advert image

--- src/components/advertIII/AdvertIII.jsx
+++ src/components/advertIII/AdvertIII.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './advertIII.css'
+import Virtual from '../../assets/advertIII/VirtualID.jpg'
+import Web from '../../assets/advertIII/Web-Banner-Week1.jpg'
 
 const AdvertIII = () => {
     return (
-        <section className='Advert__III'>
-            <h1>Hello</h1>
-        </section>
+        <div className="advert___III__section">
+            <div className="advert__container__section">
+                <div className="advert__image__section__1">
+                    <img src={Virtual} alt="" />
+                </div>
+                <div className="advert__image__section__2">
+                    <img src={Web} alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
 
